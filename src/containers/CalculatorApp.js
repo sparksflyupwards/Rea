b889954307e00587961c65b19722cd063c668b13
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-03-13 16:13:11 -0500

    sizing of the buttons and grid implemented for mobile form factor, starting to implement 3d press effects

--- src/containers/CalculatorApp.js
+++ src/containers/CalculatorApp.js
@@ -41,39 +41,39 @@ export default class CalculatorApp extends React.Component {
               handleThemeToggle={this.handleThemeToggle}
             />
             <DisplayPanel theme={this.state.currentBaseColor}/>
+            <div className="numberPad">
+                        <InputButton id="clear" class="inputButton" buttonText="AC" theme={this.state.currentBaseColor}/>
+                        <InputButton id="clearLine" class="inputButton" buttonText="C" theme={this.state.currentBaseColor}/>
+                        <InputButton id="divide" class="inputButton" buttonText="/" theme={this.state.currentBaseColor}/>
+                        <InputButton id="multiply" class="inputButton" buttonText="X" theme={this.state.currentBaseColor}/>
 
-            <InputButton id="clear" class="inputButton" buttonText="AC" theme={this.state.currentBaseColor}/>
-            <InputButton id="clearLine" class="inputButton" buttonText="C" theme={this.state.currentBaseColor}/>
-            <InputButton id="divide" class="inputButton" buttonText="/" theme={this.state.currentBaseColor}/>
-            <InputButton id="multiply" class="inputButton" buttonText="X" theme={this.state.currentBaseColor}/>
+                    
 
-            <p></p>
+                        <InputButton id="seven" class="inputButton" buttonText="7" theme={this.state.currentBaseColor}/>
+                        <InputButton id="eight" class="inputButton" buttonText="8" theme={this.state.currentBaseColor}/>
+                        <InputButton id="nine" class="inputButton" buttonText="9" theme={this.state.currentBaseColor}/>
+                        <InputButton id="subtract" class="inputButton" buttonText="-" theme={this.state.currentBaseColor}/>
 
-            <InputButton id="seven" class="inputButton" buttonText="7" theme={this.state.currentBaseColor}/>
-            <InputButton id="eight" class="inputButton" buttonText="8" theme={this.state.currentBaseColor}/>
-            <InputButton id="nine" class="inputButton" buttonText="9" theme={this.state.currentBaseColor}/>
-            <InputButton id="subtract" class="inputButton" buttonText="-" theme={this.state.currentBaseColor}/>
+                       
 
-            <p></p>
+                        <InputButton id="four" class="inputButton" buttonText="4" theme={this.state.currentBaseColor}/>
+                        <InputButton id="five" class="inputButton" buttonText="5" theme={this.state.currentBaseColor}/>
+                        <InputButton id="six" class="inputButton" buttonText="6" theme={this.state.currentBaseColor}/>
+                        <InputButton id="add" class="inputButton" buttonText="+" theme={this.state.currentBaseColor}/>
+                        
+                       
 
-            <InputButton id="four" class="inputButton" buttonText="4" theme={this.state.currentBaseColor}/>
-            <InputButton id="five" class="inputButton" buttonText="5" theme={this.state.currentBaseColor}/>
-            <InputButton id="six" class="inputButton" buttonText="6" theme={this.state.currentBaseColor}/>
-            <InputButton id="add" class="inputButton" buttonText="+" theme={this.state.currentBaseColor}/>
-            
-            <p></p>
+                        <InputButton id="one" class="inputButton" buttonText="1" theme={this.state.currentBaseColor}/>
+                        <InputButton id="two" class="inputButton" buttonText="2" theme={this.state.currentBaseColor}/>
+                        <InputButton id="three" class="inputButton" buttonText="3" theme={this.state.currentBaseColor}/>
+                        <InputButton id="equals" class="bigInputButton" buttonText="=" theme={this.state.currentBaseColor}/>
+                        
+                     
 
-            <InputButton id="one" class="inputButton" buttonText="1" theme={this.state.currentBaseColor}/>
-            <InputButton id="two" class="inputButton" buttonText="2" theme={this.state.currentBaseColor}/>
-            <InputButton id="three" class="inputButton" buttonText="3" theme={this.state.currentBaseColor}/>
-            <InputButton id="equals" class="bigInputButton" buttonText="=" theme={this.state.currentBaseColor}/>
-            
-            <p></p>
-
-            <InputButton id="?" class="inputButton" buttonText="?" theme={this.state.currentBaseColor}/>
-            <InputButton id="zero" class="inputButton" buttonText="0" theme={this.state.currentBaseColor}/>
-            <InputButton id="decimal" class="inputButton" buttonText="." theme={this.state.currentBaseColor}/>
-      
+                        <InputButton id="?" class="inputButton" buttonText="?" theme={this.state.currentBaseColor}/>
+                        <InputButton id="zero" class="inputButton" buttonText="0" theme={this.state.currentBaseColor}/>
+                        <InputButton id="decimal" class="inputButton" buttonText="." theme={this.state.currentBaseColor}/>
+                        </div>
       </div>
     );
   }
