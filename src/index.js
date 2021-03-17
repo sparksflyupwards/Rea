757cb11dcfac475131f672077c6e31cf10529ca1
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-03-17 01:01:28 -0400

    viewport test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import CalculatorApp from "./containers/CalculatorApp";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
+
     <CalculatorApp />
+    V0.0
   </StrictMode>,
   rootElement
 );
