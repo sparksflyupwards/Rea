9a9945da7d44a5943576ebfa76af158c24426748
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-03-17 22:19:37 -0400

    shadow on calculator and tweaks on button shadow

--- src/containers/CalculatorApp.js
+++ src/containers/CalculatorApp.js
@@ -25,13 +25,12 @@ export default class CalculatorApp extends React.Component {
       const clickOffset_y =
         (e.clientY - window.innerHeight * 0.05) /
         calculatorElement.offsetHeight;
-      
+
       const rotateMagnitude = 25;
       const rotateX = (clickOffset_y - 0.49) * -1 * rotateMagnitude;
       const rotateY = clickOffset_x * rotateMagnitude;
-      //const rotateY = 45;
-      console.log("loc: "+clickOffset_x + " , " + clickOffset_y)
-      console.log("rotate: "+rotateX + " , " + rotateY)
+
+    
       const distanceFromCenter = Math.pow(
         Math.pow(Math.abs(clickOffset_x) - 0, 2) +
           Math.pow(Math.abs(clickOffset_y) - 0.5, 2),
@@ -39,6 +38,12 @@ export default class CalculatorApp extends React.Component {
       );
 
       const scale = 1 - 0.045 * Math.abs(1 - distanceFromCenter);
+
+        //const rotateY = 45;
+        console.log("loc: "+clickOffset_x + " , " + clickOffset_y);
+        console.log("rotate: "+rotateX + " , " + rotateY);
+        console.log("distance: " + distanceFromCenter);
+        console.log("scale: " + scale)
       const calculatorTransform =
         "scale(" +
         scale +
